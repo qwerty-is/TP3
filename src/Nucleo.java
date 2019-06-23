f07c3dbda759af347fc3416ac9ff9687a409ef5f
NicoCia <nicocia7@gmail.com>
2019-06-23 20:38:38 -0300

    HOLI

--- src/Nucleo.java
+++ src/Nucleo.java
@@ -3,28 +3,35 @@ public class Nucleo implements Runnable{
     private Monitor monitor;
     private int tipo;
     private Tarea tarea;
+    private boolean iniciar;
 
-    public Nucleo(int repeticiones, Monitor monitor, int tipo){
+    public Nucleo(int repeticiones, Monitor monitor, int tipo, boolean iniciar){
         this.repeticiones=repeticiones;
         this.monitor=monitor;
         this.tipo=tipo;
+        this.iniciar=iniciar;
+        establecerTipo();
     }
 
     @Override
     public void run(){
-        if (tipo==1){
-            tarea=new Iniciador(monitor);
-        }
-
         for (int i=0; i<repeticiones; i++){
-            if (tipo==1){
-                tarea=new Core1(monitor);
-            }
-            else tarea=new Core2(monitor);
-
-            if (tarea.ejecutar()){
+            if(iniciar){
                 tarea=new Iniciador(monitor);
+                tarea.ejecutar();
+                iniciar=false;
+                establecerTipo();
             }
+            iniciar=tarea.ejecutar();
+        }
+    }
+
+    private void establecerTipo(){
+        if (tipo==1){
+            tarea=new Core1(monitor);
+        }
+        else {
+            tarea=new Core2(monitor);
         }
     }
 }
