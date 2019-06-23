f19dc0fbf71d9e5bde2575c600aa222e5c48663b
NicoCia <nicocia7@gmail.com>
2019-06-23 18:26:13 -0300

    HOLI

--- src/Generador.java
+++ src/Generador.java
@@ -1,3 +1,22 @@
-public class Generador {
+
+public class Generador implements Runnable{
+    private int repeticiones;
+    private int T;
+    private Monitor monitor;
+
+    public Generador(int repeticiones, Monitor monitor){
+        this.repeticiones=repeticiones;
+        this.monitor=monitor;
+    }
+
+    @Override
+    public void run(){
+        for (int i=0; i<repeticiones; i++){
+            T=3;
+            monitor.disparar(T);
+
+
+        }
+    }
 
 }
