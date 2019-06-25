1b92c8dfc4e32956cf08ce34f8117835885b0895
NicoCia <nicocia7@gmail.com>
2019-06-25 03:17:50 -0300

    Todo ready :)

--- src/Politica.java
+++ src/Politica.java
@@ -5,10 +5,25 @@ public class Politica {
     private final int TRANSICIONES=15;
     private final int buffer_CPU1=4;
     private final int buffer_CPU2=10;
+    private final int T5=5;
+    private final int T8=8;
+    private final int T4=4;
+    private final int T10=10;
+    private final int Arrival_rate=3;
+    private final int Service_rate_1=6;
+    private final int Service_rate_2=9;
+    private final int T0=0;
+    private final int T12=12;
+    private final int Power_Up_Delay_1=1;
+    private final int Power_Up_Delay_2=13;
+    private final int T2=2;
+    private final int T11=11;
+    private final int Power_Down_Threshold_1=7;
+    private final int Power_Down_Threshold_2=14;
     private int[] prioridades;
 
     public Politica(){
-        prioridades= new int[]{5, 8, 4, 10, 3, 6, 9, 0, 12, 1, 13, 2, 11, 7, 14};
+        prioridades= new int[]{T5, T8, T4, T10, Arrival_rate, Service_rate_1, Service_rate_2, T0, T12, Power_Up_Delay_1, Power_Up_Delay_2, T2, T11, Power_Down_Threshold_1, Power_Down_Threshold_2};
     }
 
     public int dondeGuardo(){
